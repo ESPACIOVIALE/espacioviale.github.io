e75bb1b6e93e6a842bf91c7b1d989380f49616c0
ESPACIOVIALE <149541065+ESPACIOVIALE@users.noreply.github.com>
2023-11-03 17:59:43 -0300

    Add files via upload

---
+++ js/wb18_barbershop.js
@@ -0,0 +1,4 @@
+$(document).ready(function()
+{
+   $("#PanelMenu1").panel({animate: true, animationDuration: 200, animationEasing: 'linear', dismissible: false, display: 'overlay', position: 'left', toggle: true});
+});
